f06d0d886ead6af100263407ccdb6c8ff34b1cb0
Arifin <ar.wijaya1221@gmail.com>
2025-03-15 18:34:03 +0700

    Commit

--- src/pertemuan01/Percobaan/percobaan02.java
+++ src/pertemuan01/Percobaan/percobaan02.java
@@ -10,12 +10,9 @@ package pertemuan01.Percobaan;
  * @author Lab256-14
  */
 public class percobaan02 {
-//Tanpa Parameter
-
     public static void sapaPengguna() {
         System.out.println("Halo, Selamat datang di pemograman Java!");
     }
-
     public static void menuMakanan() {
         System.out.println("==== Menu Makanan ===="
                 + "\n 1. Nasi Goreng"
@@ -24,7 +21,6 @@ public class percobaan02 {
                 + "\n 4. Bakso"
                 + "\n ======================");
     }
-
     public static void angkaGenap() {
         for (int i = 1; i <= 10; i++) {
             if (i % 2 == 0) {
@@ -32,7 +28,6 @@ public class percobaan02 {
             }
         }
     }
-
     public static void main(String[] args) {
         sapaPengguna();
         System.out.println("");
